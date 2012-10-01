3be213ada649be6e72f2f0fac4b090603d1acb76
Rob Levy <robert.levy@drakerlabs.com>
2012-10-01 16:55:52 -0400

    using stupid non-fixed-versioned java libraries makes you transitively stupid

--- project.clj
+++ project.clj
@@ -2,6 +2,15 @@
   :description "A Leiningen plugin to push application uberjars to AWS s3"
   :url "https://clojars.org/lein-deploy-app"
   :dependencies [[org.clojure/clojure "1.4.0"]
+
+                 ;; temporary fix to deal with terrible java developers who
+                 ;; don't depend on fixed-versioned libraries
+                 [clj-aws-s3 "0.3.2"
+                  :exclusions [org.codehaus.jackson/jackson-core-asl
+                               org.codehaus.jackson/jackson-mapper-asl]]
+                 [org.codehaus.jackson/jackson-core-asl "1.7.3"]
+                 [org.codehaus.jackson/jackson-mapper-asl "1.7.3"]
+
                  [clj-aws-s3 "0.3.2"]
                  [dsabanin-clj-jgit "0.1.3"]]
   :license {:name "Eclipse Public License"
