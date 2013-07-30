39e83cb5ee3e1ddd55ba7794cb065144edb681bf
Robert Levy <r.p.levy@gmail.com>
2013-07-30 02:23:29 -0700

    change to fix issue #4 (not tested)

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject lein-deploy-app "0.2.0"
   :description "A Leiningen plugin to push application uberjars to AWS s3"
   :url "https://clojars.org/lein-deploy-app"
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
 
                  ;; temporary fix to deal with a transitive dependency
                  ;; that does not depend on fixed-versioned libraries.
-                 [clj-aws-s3 "0.3.2"
+                 [clj-aws-s3 "0.3.6"
                   :exclusions [org.codehaus.jackson/jackson-core-asl
                                org.codehaus.jackson/jackson-mapper-asl]]
                  [org.codehaus.jackson/jackson-core-asl "1.7.3"]
                  [org.codehaus.jackson/jackson-mapper-asl "1.7.3"]
 
+                 [me.raynes/fs "1.4.5"]
                  [dsabanin/clj-jgit "0.2"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
